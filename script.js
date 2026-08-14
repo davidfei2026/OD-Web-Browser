57ad68ea1f1c824e06c1b30f77224fef5721e4a7
davidfei2026 <feidavid81022@gmail.com>
2026-08-14 18:23:59 -0500

    Implement navigation buttons in script.js
    Added functionality for navigation buttons in the web browser.

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("Hello from OD Web Browser!");
+const urlInput = document.getElementById('url');
+const goButton = document.getElementById('go');
+const backButton = document.getElementById('back');
+const forwardButton = document.getElementById('forward');
+const reloadButton = document.getElementById('reload');
+const browserFrame = document.getElementById('browser');
+
+goButton.addEventListener('click', () => {
+    const url = urlInput.value;
+    browserFrame.src = url;
+});
+
+backButton.addEventListener('click', () => {
+    browserFrame.contentWindow.history.back();
+});
+
+forwardButton.addEventListener('click', () => {
+    browserFrame.contentWindow.history.forward();
+});
+
+reloadButton.addEventListener('click', () => {
+    browserFrame.contentWindow.location.reload();
+});
